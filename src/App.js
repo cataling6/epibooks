f40a15df4dfe14920f0d1c970477056feebaabe8
Catalin Radoi <114146323+cataling6@users.noreply.github.com>
2024-02-26 21:49:02 +0100

    up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
-import Reacts from 'react'
+import Reacts, { useState } from 'react'
 import MyNav from './components/MyNav'
 import MyFooter from './components/MyFooter'
 import Welcome from './components/Welcome'
@@ -12,10 +12,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Homepage from './pages/Homepage'
 import Login from './pages/Login'
 import ProtectedRoutes from './middleware/ProtectedRoutes'
+import NotFound from './pages/NotFound'
 //import Login from './pages/Login'
 //import { SelectedProvider } from './context/SelectedContext'
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState('');
   return (
     // <>
     //   <BookProvider>
@@ -33,13 +35,19 @@ function App() {
     // </>
     <>
       <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<Login />} />
-          <Route element={<ProtectedRoutes />} >
+        <MyNav searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+        <Container>
+          <Welcome />
+          <Routes>
+            <Route exact path="/" element={<AllTheBooks searchQuery={searchQuery} />} />
+            <Route path='*' element={<NotFound />} />
+            {/* <Route element={<ProtectedRoutes />} >
             <Route path="/home" element={<Homepage />} />
-          </Route>
+          </Route> */}
 
-        </Routes>
+          </Routes>
+        </Container>
+        <MyFooter />
       </BrowserRouter >
     </>
   )
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NotFound() {
+    return (
+        <div>
+            Non ho trovato nulla
+        </div>
+    )
+}
+
+export default NotFound
