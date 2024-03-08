0f13ac44a3fb4230df69f6570ecd5d5c73566805
Catalin Radoi <radoi.catalin88@gmail.com>
2024-03-08 20:57:47 +0100

    up put & delete

--- src/components/BookDetails.jsx
+++ src/components/BookDetails.jsx
@@ -1,14 +1,29 @@
 import { useParams } from "react-router-dom"
 import { useState, useEffect } from "react";
 import { Card, Col } from "react-bootstrap";
+import Swal from "sweetalert2";
+
 function BookDetails() {
 
     const [book, setBook] = useState([]);
+    const [commentInviato, setCommentInviato] = useState(false);
     const [comments, setComments] = useState([]);
+    const [comment, setComment] = useState('');
+    const [rate, setRate] = useState('');
     const [error, setError] = useState('')
     const { ASIN } = useParams()
+
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWFlZTRkNWJkNWQxMjAwMTg5MGQzMmYiLCJpYXQiOjE3MDk5MjQ5MTEsImV4cCI6MTcxMTEzNDUxMX0.EToU0ZBKiT7mLOTdd_9ctJJfz-VHJR81AWGtXQxA98k";
     const urlGetBoook = 'https://epibooks.onrender.com/fantasy/' + ASIN;
-    const urlGetComments = `https://striveschool-api.herokuapp.com/api/books/${ASIN}/comments/`
+    const urlGetComments = `https://striveschool-api.herokuapp.com/api/books/${ASIN}/comments/`;
+    const urlSubmit = 'https://striveschool-api.herokuapp.com/api/comments';
+
+    const dataToSubmit = {
+        comment: comment,
+        rate: rate,
+        elementId: ASIN
+    };
+
 
     const getBooks = async () => {
         try {
@@ -32,16 +47,65 @@ function BookDetails() {
         }
     }
 
+    const submitComment = async (e) => {
+
+        e.preventDefault();
+
+        try {
+            const response = await fetch(urlSubmit, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+                body: JSON.stringify(dataToSubmit)
+
+            })
+            if (response.ok) {
+                console.log("dati inviati");
+                setCommentInviato(true)
+            } else {
+                setError(response.status);
+            }
+            setComment('');
+            setRate('');
+
+        } catch (er) {
+            setError(er);
+        }
+    }
+
     useEffect(() => {
         getBooks();
         getComments();
-    }, [])
+        if (error)
+            new Swal({
+                title: 'Errore: ' + error,
+                text: "Verifica i dati inseriti",
+                icon: 'error',
+                showLoaderOnConfirm: true,
+                willClose: () => {
+                    setError(null)
+                }
+            });
+        if (commentInviato) {
+            new Swal({
+                title: 'Inserito!',
+                text: "I tuoi dati sono stati inseriti, clicca ok per verificare!",
+                icon: 'success',
+                showLoaderOnConfirm: true,
+                willClose: () => {
+                    setError(null);
+                    setCommentInviato(false);
+                }
+            });
+        }
+    }, [comment, error])
 
     return <>
-        <div className="container d-flex">
-            <div className="col">
+        <div className="container d-flex gap-2">
+            <div className="col height-book-details border border-3 rounded-2 p-2">
                 {book.map((b) => {
-                    console.log(b);
                     return (
                         <Col lg={6} key={b.asin}>
                             <Card className='style-card'>
@@ -51,16 +115,31 @@ function BookDetails() {
                                 </div>
                             </Card >
                         </Col>
+
                     )
                 })}
+                <div className="d-flex flex-column form-control gap-2"  >
+                    <label>Voto:</label>
+                    <select value={rate} className="form-select" onChange={(e) => setRate(e.target.value)}>
+                        <option className="d-none">Scegli voto:</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                    </select>
+                    <label>Recensione:</label>
+                    <textarea className="form-control no-resize" placeholder="Recensisci..." value={comment} onChange={(e) => setComment(e.target.value)}></textarea>
+                    <input type="button" className="btn btn-danger" value="invia" onClick={submitComment} />
+                </div>
             </div>
-            <div className="col">
+            <div className="col height-book-details border border-3 rounded-2 p-2 ">
                 {comments.map((c) => {
-                    console.log(c);
                     return (
 
                         <Col lg={12} key={c._id} >
                             <div className="form-control mb-2">
+                                <div className="d-flex justify-content-end "><span className="btn btn-close custom-close-position"></span></div>
                                 <p className="fw-bold">Autore: <span className="fw-normal">{c.author}</span></p>
                                 <p className="fw-bold">Commento: <span className="fw-normal">{c.comment}</span></p>
                                 <p className="fw-bold">Valutazione: <span className="fw-normal">{c.rate}</span></p>
