1b750c36cce9d0d834b27632f88a7e1f0f8cf908
Catalin Radoi <radoi.catalin88@gmail.com>
2024-03-10 12:12:36 +0100

    asd

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -2,7 +2,7 @@ import { useSearch } from '../context/SearchContext'
 import { Col, Row } from 'react-bootstrap'
 import { BookContext } from '../context/BooksContext'
 import SingleBook from './SingleBook'
-import { useContext, useEffect, useState } from 'react'
+import { useContext } from 'react'
 import { SelectedProvider } from '../context/SelectedContext'
 
 
